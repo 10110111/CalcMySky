ec8f28ee965ff0fd47b4963312587b4439e8f05b
Ruslan Kabatsayev <b7.10110111@gmail.com>
2021-02-05 15:33:49 +0300

    Add missing 'override' keyword

--- ShowMySky/RadiancePlot.hpp
+++ ShowMySky/RadiancePlot.hpp
@@ -21,8 +21,8 @@ public:
                  float azimuth, float elevation);
 
 protected:
-    void paintEvent(QPaintEvent *event);
-    void keyPressEvent(QKeyEvent* event);
+    void paintEvent(QPaintEvent *event) override;
+    void keyPressEvent(QKeyEvent* event) override;
     void mouseMoveEvent(QMouseEvent* event) override;
     void leaveEvent(QEvent* event) override;
 
