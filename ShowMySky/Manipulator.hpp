f4a826699a8de3df7bbda11ad1f259aeb3d9d8d8
Ruslan Kabatsayev <b7.10110111@gmail.com>
2025-12-21 12:28:59 +0400

    Add a license notice in all sources and headers
    This in particular will make the license clear for the users of the
    API include files.

--- ShowMySky/Manipulator.hpp
+++ ShowMySky/Manipulator.hpp
@@ -1,3 +1,22 @@
+/*
+ * CalcMySky - a simulator of light scattering in planetary atmospheres
+ * Copyright © 2025 Ruslan Kabatsayev
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
 #ifndef INCLUDE_ONCE_66A25B12_D91A_4496_B4D2_0B1BA1B7A168
 #define INCLUDE_ONCE_66A25B12_D91A_4496_B4D2_0B1BA1B7A168
 
